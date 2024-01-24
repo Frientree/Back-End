c3ecc49521064a705c75205f540e7b26b317d1eb
이호성 <ghtjd8264@naver.com>
2024-01-24 22:45:15 +0900

    style: change code formatt by ktlint
    kt린트 사용 누락된 컨벤션 적용
    
    Change-Id: Ic86e708b5c05c390b3c6ae4a614cee7682a12ec7

--- android_repo/app/src/main/java/com/d101/frientree/di/RepositoryModule.kt
+++ android_repo/app/src/main/java/com/d101/frientree/di/RepositoryModule.kt
@@ -14,6 +14,6 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindUserRepository(
-        userRepositoryImpl: UserRepositoryImpl
+        userRepositoryImpl: UserRepositoryImpl,
     ): UserRepository
 }
