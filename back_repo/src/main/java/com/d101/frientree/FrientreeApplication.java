48473b7286408544f2e98f11fae4fa73c5b2a5f4
오하빈 <dhdudgns80@naver.com>
2024-01-25 15:49:50 +0900

    feat : application.yml file change application.yml 형식을 깔끔하게 변경했습니다.
    Change-Id: I14109985356efce66a360428a256a37ceb4c9764

--- back_repo/src/main/java/com/d101/frientree/FrientreeApplication.java
+++ back_repo/src/main/java/com/d101/frientree/FrientreeApplication.java
@@ -7,7 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class FrientreeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FrientreeApplication.class, args);
+//		SpringApplication.run(FrientreeApplication.class, args);
+		SpringApplication app = new SpringApplication(FrientreeApplication.class);
+		app.setAdditionalProfiles("development");
+		app.run(args);
 	}
 
 }
