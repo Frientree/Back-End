3cf40200a8018f73038521a7b05a2f5443019507
정수진 <dmg06017@naver.com>
2024-01-25 16:29:20 +0900

    feat: add today date feature
    Main화면에 오늘 날짜를 나타내는 기능을 추가함
    
    Change-Id: I9029b56eaff10ee16acff56617ab9cc72455b038

--- android_repo/presentation/src/main/java/com/d101/presentation/main/viewmodel/MainViewModel.kt
+++ android_repo/presentation/src/main/java/com/d101/presentation/main/viewmodel/MainViewModel.kt
@@ -1,26 +1,30 @@
 package com.d101.presentation.main.viewmodel
 
 import androidx.lifecycle.ViewModel
-import com.d101.domain.usecase.main.ChangeTreeNameUseCase
-import com.d101.domain.usecase.main.DecideTodayFruitUseCase
-import com.d101.domain.usecase.main.GetMessageFromTreeUseCase
-import com.d101.domain.usecase.main.GetRandomLeafUseCase
-import com.d101.domain.usecase.main.GetTodayFruitUseCase
-import com.d101.domain.usecase.main.MakeFruitBySpeechUseCase
-import com.d101.domain.usecase.main.MakeFruitByTextUseCase
-import com.d101.domain.usecase.main.SendMyLeafUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import java.time.LocalDate
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor(
-    private val changeTreeNameUseCase: ChangeTreeNameUseCase,
-    private val decideTodayFruitUseCase: DecideTodayFruitUseCase,
-    private val getMessageFromTreeUseCase: GetMessageFromTreeUseCase,
-    private val getRandomLeafUseCase: GetRandomLeafUseCase,
-    private val getTodayFruitUseCase: GetTodayFruitUseCase,
-    private val makeFruitBySpeechUseCase: MakeFruitBySpeechUseCase,
-    private val makeFruitByTextUseCase: MakeFruitByTextUseCase,
-    private val sendMyLeafUseCase: SendMyLeafUseCase,
-) : ViewModel() {
+class MainViewModel @Inject constructor() : ViewModel() {
+
+    private val _todayDate: MutableStateFlow<String> = MutableStateFlow("year / month / day")
+    val todayDate: StateFlow<String> = _todayDate.asStateFlow()
+
+    fun initTodayDate() {
+        val localDate: LocalDate = LocalDate.now()
+
+        _todayDate.update {
+            String.format(
+                "%d / %d / %d",
+                localDate.year,
+                localDate.monthValue,
+                localDate.dayOfMonth,
+            )
+        }
+    }
 }
