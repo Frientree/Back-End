2f37fdf23153f514ef6cb795f2cdde13314bf76a
이호성 <ghtjd8264@naver.com>
2024-01-27 01:35:06 +0900

    feat: SignInFragment set binding
    로그인 화면 데이터 바인딩 적용
    Change-Id: Ifa89938a467e4bfcc247e1e6b2e520e9e8682637

--- android_repo/presentation/src/main/java/com/d101/presentation/welcome/fragments/SignInFragment.kt
+++ android_repo/presentation/src/main/java/com/d101/presentation/welcome/fragments/SignInFragment.kt
@@ -5,15 +5,33 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.d101.presentation.R
+import com.d101.presentation.databinding.FragmentSignInBinding
 
 class SignInFragment : Fragment() {
+
+    private var _binding: FragmentSignInBinding? = null
+    private val binding
+        get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_sign_in, container, false)
+    ): View {
+        _binding = FragmentSignInBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setBinding()
+    }
+
+    private fun setBinding() {
+        binding.lifecycleOwner = viewLifecycleOwner
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
