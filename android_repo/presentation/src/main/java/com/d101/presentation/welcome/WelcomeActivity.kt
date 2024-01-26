7daebbdf8795613ba4c2dca6994cf14aa5a0a32b
이호성 <ghtjd8264@naver.com>
2024-01-26 23:53:05 +0900

    feat: create WelcomeActivity
    회원가입, 로그인, 약관 등 화면이 포함 될 액티비티 생성
    Change-Id: I606d46c9bafc726de5edf3a6a41f8ce638e5d174

---
+++ android_repo/presentation/src/main/java/com/d101/presentation/welcome/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.d101.presentation.welcome
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.d101.presentation.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
