227eb803be5e5207be191300d4b1461c6a7f0618
이호성 <ghtjd8264@naver.com>
2024-01-27 01:44:56 +0900

    feat: SignInFragment navigate
    회원가입 화면 이동 흐름 임시 구현

--- android_repo/presentation/src/main/java/com/d101/presentation/welcome/fragments/SignInFragment.kt
+++ android_repo/presentation/src/main/java/com/d101/presentation/welcome/fragments/SignInFragment.kt
@@ -1,11 +1,15 @@
 package com.d101.presentation.welcome.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.d101.presentation.R
 import com.d101.presentation.databinding.FragmentSignInBinding
+import com.d101.presentation.main.MainActivity
 
 class SignInFragment : Fragment() {
 
@@ -25,11 +29,27 @@ class SignInFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setBinding()
+
+        // 임시 화면이동 로직 추후 재정의 후 제거
+        binding.signInButton.setOnClickListener {
+            val intent = Intent(requireContext(), MainActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+        }
+
+        binding.signUpTextView.setOnClickListener {
+            findNavController().navigate(R.id.action_signInFragment_to_termsAgreeFragment)
+        }
+
+        binding.findPasswordTextView.setOnClickListener {
+            findNavController().navigate(R.id.action_signInFragment_to_findPasswordFragment)
+        }
     }
 
     private fun setBinding() {
         binding.lifecycleOwner = viewLifecycleOwner
     }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
