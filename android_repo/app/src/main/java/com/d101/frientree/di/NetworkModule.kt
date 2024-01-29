5af1c462222bec4f8022525b4c76a8fdc8b90945
이호성 <ghtjd8264@naver.com>
2024-01-30 01:07:31 +0900

    feat: add accessToken header Interceptor
    헤더에 accessToken다는 인터셉터 추가
    
    Change-Id: Idd8d0ca5f193a4ce8de4a071c1c70f307c42672d

--- android_repo/app/src/main/java/com/d101/frientree/di/NetworkModule.kt
+++ android_repo/app/src/main/java/com/d101/frientree/di/NetworkModule.kt
@@ -1,9 +1,14 @@
 package com.d101.frientree.di
 
+import androidx.datastore.core.DataStore
+import com.d101.data.datastore.TokenPreferences
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.converter.gson.GsonConverterFactory
@@ -15,12 +20,15 @@ object NetworkModule {
 
     @Singleton
     @Provides
-    fun provideFrientreeClient(): OkHttpClient {
+    fun provideFrientreeClient(
+        authorizationInterceptor: Interceptor,
+    ): OkHttpClient {
         val builder = OkHttpClient.Builder()
         val loggingInterceptor = HttpLoggingInterceptor()
         loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
         builder.apply {
             addInterceptor(loggingInterceptor)
+            addInterceptor(authorizationInterceptor)
         }
         return builder.build()
     }
@@ -29,5 +37,22 @@ object NetworkModule {
     @Provides
     fun provideConverterFactory(): GsonConverterFactory {
         return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun providesAuthorizationInterceptor(
+        tokenPreferencesStore: DataStore<TokenPreferences>,
+    ) = Interceptor { chain ->
+        var request = chain.request()
+
+        val accessToken = runBlocking { tokenPreferencesStore.data.first() }.accessToken
+
+        if (accessToken.isNotEmpty()) {
+            request = chain.request().newBuilder()
+                .addHeader("Authorization", "Bearer $accessToken")
+                .build()
+        }
+        chain.proceed(request)
     }
 }
