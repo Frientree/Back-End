ee55b8c23f4a1672366452493894211cb4f006cd
이호성 <ghtjd8264@naver.com>
2024-01-29 17:55:52 +0900

    feat: save token data
    로그인 성공 시 token data 저장
    
    Change-Id: Ide5234355d74dd1f15657feaec3f84b1c567801d

--- android_repo/data/src/main/java/com/d101/data/repository/UserRepositoryImpl.kt
+++ android_repo/data/src/main/java/com/d101/data/repository/UserRepositoryImpl.kt
@@ -1,19 +1,29 @@
 package com.d101.data.repository
 
+import androidx.datastore.core.DataStore
 import com.d101.data.datasource.user.UserDataSource
+import com.d101.data.datastore.TokenPreferences
 import com.d101.domain.model.Result
 import com.d101.domain.repository.UserRepository
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
+    private val tokenPreferencesStore: DataStore<TokenPreferences>,
     private val userDataSource: UserDataSource,
 ) : UserRepository {
     override suspend fun signIn(userId: String, userPw: String) =
         when (val result = userDataSource.signIn(userId, userPw)) {
             is Result.Success -> {
-                // 데이터 스토에서 토큰 저장
+                tokenPreferencesStore.updateData { tokenData ->
+                    tokenData.toBuilder()
+                        .setAccessToken(result.data.accessToken)
+                        .setRefreshToken(result.data.refreshToken)
+                        .build()
+                }
+
                 Result.Success(true)
             }
+
             is Result.Failure -> Result.Failure(result.errorStatus)
         }
 }
