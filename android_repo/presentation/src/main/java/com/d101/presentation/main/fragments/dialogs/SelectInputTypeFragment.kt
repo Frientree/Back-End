ffb7c33f1acb828cfbef0a4c856e530b64eb59f2
정수진 <dmg06017@naver.com>
2024-01-30 12:49:06 +0900

    feat: add record permission
    녹음 권한 요청하는 기능 추가
    
    Change-Id: I7e8c1448fb3a20f02f4f9c84f93c5cd70678c7dd

--- android_repo/presentation/src/main/java/com/d101/presentation/main/fragments/dialogs/SelectInputTypeFragment.kt
+++ android_repo/presentation/src/main/java/com/d101/presentation/main/fragments/dialogs/SelectInputTypeFragment.kt
@@ -1,19 +1,36 @@
 package com.d101.presentation.main.fragments.dialogs
 
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.d101.presentation.R
 import com.d101.presentation.databinding.FragmentSelectInputTypeBinding
+import com.d101.presentation.main.MainActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class SelectInputTypeFragment : Fragment() {
     private var _binding: FragmentSelectInputTypeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var activity: MainActivity
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        activity = context as MainActivity
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -33,17 +50,58 @@ class SelectInputTypeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.speechLottieView.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.before_fragment_container_view, FruitCreationBySpeechFragment())
-                .addToBackStack(null)
-                .commit()
+            requestAudioPermission()
         }
         binding.textTextView.setOnClickListener {
-            parentFragmentManager.beginTransaction()
-                .replace(R.id.before_fragment_container_view, FruitCreationByTextFragment())
-                .addToBackStack(null)
-                .commit()
+            moveToFragment(FruitCreationByTextFragment())
         }
+    }
+
+    private fun requestAudioPermission() {
+        if (ActivityCompat.checkSelfPermission(
+                activity,
+                Manifest.permission.RECORD_AUDIO,
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Requesting permission to RECORD_AUDIO
+            val permissions: Array<String> = arrayOf(Manifest.permission.RECORD_AUDIO)
+            requestPermissionLauncher.launch(permissions)
+        } else {
+            moveToFragment(FruitCreationBySpeechFragment())
+        }
+    }
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions(),
+    ) { permissions ->
+        permissions.entries.forEach { (permission, granted) ->
+            if (granted) {
+                // 권한이 허용된 경우
+                moveToFragment(FruitCreationBySpeechFragment())
+            } else {
+                // 권한이 거부된 경우
+                if (shouldShowRequestPermissionRationale(permission)) {
+
+                    FruitDialogInterface.dialog.dismiss()
+                    Toast.makeText(activity, "권한을 허용해야 말하기 기능을 이용할 수 있습니다.", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    // '다시 보지 않기' 선택 또는 기타 상황 처리
+                    Log.i("Permission", "Permission denied without ask again: $permission")
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", activity.packageName, null)
+                    intent.data = uri
+                    startActivity(intent)
+                }
+            }
+        }
+    }
+
+    private fun moveToFragment(destinationFragment: Fragment) {
+        parentFragmentManager.beginTransaction()
+            .replace(R.id.before_fragment_container_view, destinationFragment)
+            .addToBackStack(null)
+            .commit()
     }
 
     override fun onDestroyView() {
