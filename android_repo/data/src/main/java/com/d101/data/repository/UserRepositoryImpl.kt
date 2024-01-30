43c4a6ee3413259e24df32416678835a6b080ec6
이호성 <ghtjd8264@naver.com>
2024-01-31 01:02:02 +0900

    feat: get User Info logic
    도메인에 User 클래스 생성 및 mapper 구현
    로컬에서 사용자 정보 조회 후 없을 시 서버에 요청
    
    Change-Id: Ic1b872e58ec65af484de4c2d87f5c9d35ebdd7e3

--- android_repo/data/src/main/java/com/d101/data/repository/UserRepositoryImpl.kt
+++ android_repo/data/src/main/java/com/d101/data/repository/UserRepositoryImpl.kt
@@ -2,24 +2,27 @@ package com.d101.data.repository
 
 import androidx.datastore.core.DataStore
 import com.d101.data.datasource.user.UserDataSource
-import com.d101.data.datastore.TokenPreferences
+import com.d101.data.datastore.UserPreferences
+import com.d101.data.mapper.UserMapper.toUser
 import com.d101.domain.model.Result
+import com.d101.domain.model.User
 import com.d101.domain.repository.UserRepository
+import com.d101.domain.utils.TokenManager
+import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
-    private val tokenPreferencesStore: DataStore<TokenPreferences>,
+    private val tokenManager: TokenManager,
+    private val userDataStore: DataStore<UserPreferences>,
     private val userDataSource: UserDataSource,
 ) : UserRepository {
     override suspend fun signIn(userId: String, userPw: String) =
         when (val result = userDataSource.signIn(userId, userPw)) {
             is Result.Success -> {
-                tokenPreferencesStore.updateData { tokenData ->
-                    tokenData.toBuilder()
-                        .setAccessToken(result.data.accessToken)
-                        .setRefreshToken(result.data.refreshToken)
-                        .build()
-                }
+                tokenManager.saveToken(
+                    result.data.accessToken,
+                    result.data.refreshToken,
+                )
 
                 Result.Success(Unit)
             }
@@ -27,13 +30,30 @@ class UserRepositoryImpl @Inject constructor(
             is Result.Failure -> Result.Failure(result.errorStatus)
         }
 
-    override suspend fun getUserInfo(): Result<Unit> =
-        when (val result = userDataSource.getUserInfo()) {
+    override suspend fun getUserInfo(): Result<User> {
+        val localUserInfo = userDataStore.data.first()
+
+        if (localUserInfo.userEmail.isNotEmpty()) {
+            return Result.Success(localUserInfo.toUser())
+        }
+
+        return when (val result = userDataSource.getUserInfo()) {
             is Result.Success -> {
-                // 유저정보 저장
-                Result.Success(Unit)
+                userDataStore.updateData {
+                    result.data.let {
+                        UserPreferences.newBuilder()
+                            .setUserEmail(it.userEmail)
+                            .setUserNickname(it.userNickname)
+                            .setUserLeafStatus(it.userLeafStatus)
+                            .setUserNotification(it.userNotification)
+                            .setUserFruitStatus(it.userFruitStatus)
+                            .build()
+                    }
+                }
+                Result.Success(result.data.toUser())
             }
 
             is Result.Failure -> Result.Failure(result.errorStatus)
         }
+    }
 }
--- android_repo/domain/src/main/java/com/d101/domain/repository/UserRepository.kt
+++ android_repo/domain/src/main/java/com/d101/domain/repository/UserRepository.kt
@@ -1,9 +1,10 @@
 package com.d101.domain.repository
 
 import com.d101.domain.model.Result
+import com.d101.domain.model.User
 
 interface UserRepository {
     suspend fun signIn(userId: String, userPw: String): Result<Unit>
 
-    suspend fun getUserInfo(): Result<Unit>
+    suspend fun getUserInfo(): Result<User>
 }
