23caeaed83a7133c2b875d086e6432b193410539
이호성 <ghtjd8264@naver.com>
2024-02-05 16:12:58 +0900

    refactor: Connecting termsData to Server
    Change-Id: I03fdf80fef30c57a09af0f2ba706672025ad1242

--- android_repo/data/src/main/java/com/d101/data/datasource/terms/TermsDataSourceImpl.kt
+++ android_repo/data/src/main/java/com/d101/data/datasource/terms/TermsDataSourceImpl.kt
@@ -3,32 +3,23 @@ package com.d101.data.datasource.terms
 import com.d101.data.api.TermsService
 import com.d101.data.model.terms.response.TermsResponse
 import com.d101.domain.model.Result
+import com.d101.domain.model.status.ErrorStatus
+import java.io.IOException
 import javax.inject.Inject
 
 class TermsDataSourceImpl @Inject constructor(
     private val termsService: TermsService,
 ) : TermsDataSource {
-    override suspend fun getTerms(): Result<List<TermsResponse>> =
-//        runCatching {
-//            termsService.getTerms().getOrThrow().data
-//        }.fold(
-//            onSuccess = { Result.Success(it) },
-//            onFailure = { e ->
-//                if (e is IOException) {
-//                    Result.Failure(ErrorStatus.NetworkError)
-//                } else {
-//                    Result.Failure(ErrorStatus.UnknownError)
-//                }
-//            },
-//        )
-        Result.Success(
-            List(3) {
-                val necessary = if (it < 2) "[필수]" else "[선택]"
-                TermsResponse(
-                    name = "$necessary ${it}번째 약관",
-                    url = "https://www.google.com",
-                    necessary = it < 2,
-                )
-            },
-        )
+    override suspend fun getTerms(): Result<List<TermsResponse>> = runCatching {
+        termsService.getTerms().getOrThrow().data
+    }.fold(
+        onSuccess = { Result.Success(it) },
+        onFailure = { e ->
+            if (e is IOException) {
+                Result.Failure(ErrorStatus.NetworkError)
+            } else {
+                Result.Failure(ErrorStatus.UnknownError)
+            }
+        },
+    )
 }
