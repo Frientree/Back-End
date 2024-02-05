27f172be8fd8e5eeb6cee711b6898dff54337de5
이호성 <ghtjd8264@naver.com>
2024-02-05 17:53:18 +0900

    feat: find password ui logic
    비밀번호 찾기 요청 ui 흐름
    
    Change-Id: I0af4eddd21d75c47ae78a3722be5a269381b291b

--- android_repo/presentation/src/main/java/com/d101/presentation/welcome/fragments/FindPasswordFragment.kt
+++ android_repo/presentation/src/main/java/com/d101/presentation/welcome/fragments/FindPasswordFragment.kt
@@ -5,15 +5,51 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.d101.presentation.R
+import androidx.fragment.app.viewModels
+import com.d101.presentation.databinding.FragmentFindPasswordBinding
+import com.d101.presentation.welcome.viewmodel.FindPasswordViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import utils.repeatOnStarted
 
+@AndroidEntryPoint
 class FindPasswordFragment : Fragment() {
+
+    private var _binding: FragmentFindPasswordBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: FindPasswordViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?,
-    ): View? {
-        return inflater.inflate(R.layout.fragment_find_password, container, false)
+    ): View {
+        _binding = FragmentFindPasswordBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setBinding()
+        collectUiState()
+    }
+
+    private fun setBinding() {
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+    }
+
+    private fun collectUiState() {
+        viewLifecycleOwner.repeatOnStarted {
+            viewModel.uiState.collect {
+                binding.emailInputLayout.setInputDataState(it)
+            }
+        }
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
---
+++ android_repo/presentation/src/main/java/com/d101/presentation/welcome/viewmodel/FindPasswordViewModel.kt
@@ -0,0 +1,59 @@
+package com.d101.presentation.welcome.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.d101.domain.usecase.usermanagement.FindPasswordUseCase
+import com.d101.presentation.R
+import com.d101.presentation.welcome.model.DescriptionType
+import com.d101.presentation.welcome.state.InputDataState
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import utils.RegexPattern.EMAIL_PATTERN
+import javax.inject.Inject
+
+@HiltViewModel
+class FindPasswordViewModel @Inject constructor(
+    private val findPasswordUseCase: FindPasswordUseCase
+) : ViewModel() {
+
+    private val _uiState: MutableStateFlow<InputDataState.IdInputState> =
+        MutableStateFlow(InputDataState.IdInputState(
+            buttonClick = {}
+        ))
+
+    val uiState = _uiState.asStateFlow()
+
+    val email: MutableStateFlow<String> = MutableStateFlow("")
+
+
+    init {
+        collectEmailChange()
+    }
+
+    private fun collectEmailChange() {
+        viewModelScope.launch {
+            email.collect { email ->
+                _uiState.update { currentState ->
+                    if (isEmailValid(email)) {
+                        currentState.copy(
+                            buttonEnabled = true,
+                            description = R.string.empty_text,
+                        )
+                    } else {
+                        currentState.copy(
+                            buttonEnabled = false,
+                            descriptionType = DescriptionType.ERROR,
+                            description = R.string.check_email_form,
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+    fun isEmailValid(email: String) = email.matches(EMAIL_PATTERN.toRegex())
+
+}
