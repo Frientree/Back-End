ba25f81c9499ef789e8d035a26ee582cfc5a86c2
이호성 <ghtjd8264@naver.com>
2024-02-05 21:24:15 +0900

    feat: create date interceptor
    Date 헤더 요청에 날짜 추가
    
    Change-Id: I2e81dd2b2cdc0abfe3227b2279f9ea67dee60932

---
+++ android_repo/data/src/main/java/com/d101/data/utils/DateInterceptor.kt
@@ -0,0 +1,20 @@
+package com.d101.data.utils
+
+
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.time.LocalDate
+import javax.inject.Inject
+
+class DateInterceptor @Inject constructor() : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        var request = chain.request()
+        if (request.headers.names().contains("Date")) {
+            request = chain.request().newBuilder()
+                .addHeader("Date", LocalDate.now().toString())
+                .build()
+        }
+
+        return chain.proceed(request)
+    }
+}
