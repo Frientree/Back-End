9f2866e1b0b713f68acdf84feca42cea21cb8f69
정수진 <dmg06017@naver.com>
2024-02-07 10:11:58 +0900

    fix: leaf api request
    leaf request 모델 수정 및 린트
    
    Change-Id: Ia05375236d2ab6ebd7a7d19b81cc59e3c4ba38d2

--- android_repo/data/src/main/java/com/d101/data/model/leaf/request/LeafCreationRequest.kt
+++ android_repo/data/src/main/java/com/d101/data/model/leaf/request/LeafCreationRequest.kt
@@ -1,6 +1,6 @@
 package com.d101.data.model.leaf.request
 
 data class LeafCreationRequest(
-    val category: Int,
-    val content: String,
+    val leafCategory: Int,
+    val leafContent: String,
 )
