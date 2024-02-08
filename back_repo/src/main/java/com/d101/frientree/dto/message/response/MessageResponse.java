94352bb87f8fa69a96629823485890d26f269a23
오하빈 <dhdudgns80@naver.com>
2024-02-08 13:43:14 +0900

    feat : message swagger
    message swagger 작업 완료
    
    Change-Id: I2755fae1fc5efad75c0a9711fcde8edab8999510

--- back_repo/src/main/java/com/d101/frientree/dto/message/response/MessageResponse.java
+++ back_repo/src/main/java/com/d101/frientree/dto/message/response/MessageResponse.java
@@ -1,4 +1,5 @@
 package com.d101.frientree.dto.message.response;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,7 +10,9 @@ import lombok.Data;
 @AllArgsConstructor
 
 public class MessageResponse {
+    @Schema(description = "상태 코드", example = "Success")
     private String message;
+    @Schema(description = "나무 메시지 내용")
     private String data;
 
     public static MessageResponse createMessageResponse(String message, String description) {
