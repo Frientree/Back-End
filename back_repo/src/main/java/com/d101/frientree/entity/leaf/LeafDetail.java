3495f0aced16ace341362806c437ab02774924ee
오하빈 <dhdudgns80@naver.com>
2024-02-12 12:09:24 +0900

    dev : leafDetail modify
    이파리 entity 주석 제거
    
    Change-Id: Ia1cd22e96a08067f6c9ec03660b6ddff7898465e

--- back_repo/src/main/java/com/d101/frientree/entity/leaf/LeafDetail.java
+++ back_repo/src/main/java/com/d101/frientree/entity/leaf/LeafDetail.java
@@ -22,13 +22,8 @@ public class LeafDetail {
     @Column(name = "leaf_content")
     private String leafContent;
 
-//    @Enumerated(EnumType.STRING)
-//    @Column(name = "leaf_category")
-//    private LeafCategory leafCategory;
-
     @Column(name = "leaf_category")
     private String leafCategory;
-
 
     @Column(name = "leaf_complain")
     @Builder.Default
