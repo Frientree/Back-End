8470821e52f48cc35415c3bc7bc055c27272db7d
오하빈 <dhdudgns80@naver.com>
2024-02-14 09:51:11 +0900

    master : app url
    긴급! 앱 스토어 업데이트 URL 추가!
    
    Change-Id: Iab26d6d20c113ceafaeb98b9fb5e3206fd998ed7

--- back_repo/src/main/java/com/d101/frientree/dto/appversion/dto/AppVersionResponseDTO.java
+++ back_repo/src/main/java/com/d101/frientree/dto/appversion/dto/AppVersionResponseDTO.java
@@ -17,7 +17,7 @@ public class AppVersionResponseDTO {
     @Schema(description = "앱 최소 실행 버전", example = "Success")
     private String minVersion;
 
-    private static String updateUrl = "www.naver.com";
+    private static String updateUrl = "https://play.google.com/store/apps/details?id=com.d101.frientree";
 
     public static AppVersionResponseDTO createAppVersionResponseDTO(AppVersion appVersion) {
         return AppVersionResponseDTO.builder()
