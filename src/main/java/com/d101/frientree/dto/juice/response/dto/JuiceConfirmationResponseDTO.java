139694ad745f7c684d894b9508cd3cbc2f9bb408
HABIN <dhdudgns80@naver.com>
2024-04-07 20:15:54 +0900

    hotfix : image background image url add
    주스 도감 조회 시 백그라운드 이미지 url 추가

--- src/main/java/com/d101/frientree/dto/juice/response/dto/JuiceConfirmationResponseDTO.java
+++ src/main/java/com/d101/frientree/dto/juice/response/dto/JuiceConfirmationResponseDTO.java
@@ -15,6 +15,8 @@ public class JuiceConfirmationResponseDTO {
     private String juiceName;
     @Schema(description = "주스 세부 내용", example = "달콤하고 바삭한 사과의 맛이 어우러진 사과 주스입니다. 상큼하면서도 과일 특유의 달콤한 맛이 느껴집니다.")
     private String juiceDescription;
+    @Schema(description = "주스 뒷 배경 URL", example = "www.background.com")
+    private String juiceBackgroundImageUrl;
 
     public static JuiceConfirmationResponseDTO createJuiceConfirmationResponseDTO(JuiceDetail juiceDetail) {
         return JuiceConfirmationResponseDTO.builder()
